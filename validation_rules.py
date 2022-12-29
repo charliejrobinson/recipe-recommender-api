4fb0ae88ab56f70895c8049449830f51161b3c19
Charlie Robinson <charliejrobinson1@gmail.com>
2022-12-29 18:32:14 +0000

    Fixed validation rule

--- validation_rules.py
+++ validation_rules.py
@@ -1,7 +1,7 @@
 from validator import validate
 
 put_recipe_rules = {
-    'name': 'required|alpha', # required name, alphabetic chars only
+    'name': 'required|string', # required name, alphabetic chars only
     'ingredients': 'required|list', # required ingredients, must be list
-    'instructions': 'required|string'
+    'instructions': 'required|string' # required instructions, string
 }
