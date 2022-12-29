7f598823317b91b640e62bdc3da68de879995144
Charlie Robinson <charliejrobinson1@gmail.com>
2022-12-29 20:33:54 +0000

    Added /id path to pathway

--- call_api.py
+++ call_api.py
@@ -3,28 +3,25 @@ from pprint import pprint
 
 host_add = 'http://192.168.1.199:81'
 
-data = {
-    'name': 'recipe test 2',
-    'ingredients': ['a','b'],
-    'instructions': ['c','d']
-}
-
 # Send PUT request to create new recipe
 def put_new_recipe(data):
-    p = requests.put(f'{host_add}/recipe', json=data)
+    r = requests.put(f'{host_add}/recipe', json=data)
+    print(r.json())
 
 # Send PUT request to update recipe
-def put_update_recipe(data):
-    p = requests.put(f'{host_add}/recipe', json=data)
+def put_update_recipe(id, data):
+    r = requests.put(f'{host_add}/recipe/{id}', json=data)
+    print(r.json())
 
 # Send GET request to get a recipe from id
 def get_recipe(id):
-    return requests.get(f'{host_add}/recipe')
+    r = requests.get(f'{host_add}/recipe/{id}')
+    print(r.json())
 
 # Send DELETE request to delete a recipe from id
 def delete_recipe(id):
-    return requests.delete(f'{host_add}/recipe/{id}')
-
+    r = requests.delete(f'{host_add}/recipe/{id}')
+    print(r.json())
 
 # validate data
 # clean up code
@@ -32,3 +29,11 @@ def delete_recipe(id):
 # add comments & documentation
 # validate request : if object doesn't exist, return 404
 # vue / IFTT
+
+data = {
+    'name': 3,
+    'ingredients': ['radish','honey'],
+    'instructions': 'Put in oven'
+}
+
+get_recipe('tNk7IB6gwrm9WcPzzYat')
