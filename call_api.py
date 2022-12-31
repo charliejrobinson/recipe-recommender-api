d3d302309eea60cd2c562b9dabed782c4aa0fd98
Charlie Robinson <charliejrobinson1@gmail.com>
2022-12-31 12:36:52 +0000

    Added query route & updated request functions to return instead of print for use in other files

--- call_api.py
+++ call_api.py
@@ -6,30 +6,33 @@ host_add = 'http://192.168.1.199:81'
 # Send PUT request to create new recipe
 def create_new_recipe(data):
     r = requests.put(f'{host_add}/recipes', json=data)
-    print(r.json())
+    return r.json()
 
 # Send PUT request to update recipe
 def update_recipe(id, data):
     r = requests.put(f'{host_add}/recipe/{id}', json=data)
-    print(r.json())
+    return r.json()
 
 # Send GET request to get a recipe from id
 def get_recipe(id):
     r = requests.get(f'{host_add}/recipe/{id}')
-    print(r.json())
+    return r.json()
 
 # Send DELETE request to delete a recipe from id
 def delete_recipe(id):
     r = requests.delete(f'{host_add}/recipe/{id}')
-    print(r.json())
+    return r.json()
 
 # Send GET request to get all recipes
 def get_all_recipes():
     r = requests.get(f'{host_add}/recipes')
-    print(r.json())
+    return r.json()
 
-# validate data
-# clean up code
+# Send GET request to search for recipe by name
+def search_by_name(recipe_name):
+    r = requests.get(f'{host_add}/recipes/search?name={recipe_name}')
+    return r.json()
+
 # add query route
 # add comments & documentation
 # validate request : if object doesn't exist, return 404
@@ -41,4 +44,4 @@ data = {
     'instructions': 'Put in oven'
 }
 
-get_all_recipes()
+search_by_name('validrecipe')
